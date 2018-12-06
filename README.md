64610b5dee108e89748f0943a2f117bda339b69d
Claudio Davi <cdavisouza@gmail.com>
2018-12-06 00:02:02 -0200

    Esqueci desses arquivos - Back end

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Objetivo: Criar um recomendador de produtos baseados nas suas hashtags
 2. Na pasta `/api` execute `npm install`
 3. Na pasta `/ui` execute `npm install`
 4. Rode localmente uma versão do Neo4j (Pode ser docker) 
-`docker run --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data neo4j`
+`docker run -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data neo4j`
 5. Configure o arquivo `/api/.env` para as informações de login da sua stack
 6. Para fazer upload dos dados de teste no Neo4j: Na pasta `/api` rode `npm run seedDb`
 7. Para abrir o console do GraphQl rode `npm start` na pasta `/api`
--- api/src/graphql-schema.js
+++ api/src/graphql-schema.js
@@ -16,6 +16,24 @@ export const typeDefs = fs
 export const resolvers = {
   Query: {
     products: neo4jgraphql,
-    hashtags: neo4jgraphql
+    hashtags: neo4jgraphql,
+    search: (root, args, context) => {
+      let session = context.driver.session();
+      let query = "MATCH(product:Product) WHERE toLower(product.name) CONTAINS toLower($filter) RETURN p LIMIT $limit;"
+      return session.run(query, args).then(
+        result => { return result.records.map(record => {return record.get("product").properties})})
+    },
+  },
+  Product: {
+    similar: (product, _, context) => {
+      let session = context.driver.session();
+      let params = {id: product.id};
+      let query = ` MATCH(product:Product) WHERE product.id = $id
+                    MATCH (product)-[:Hashtags]->(t:Tag)<-[:Hashtags]-(prod:Product)
+                    WITH product, prod, COUNT(*) AS tagOverlap
+                    RETURN prod ORDER BY(tagOverlap) DESC LIMIT 2`;
+      return session.run(query, params)
+        .then(result => {return result.records.map(record => {return record.get("prod").properties})})
+    }
   }
-}
+};
